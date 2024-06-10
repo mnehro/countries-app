315e2dcd6dfb0bc2b4bf25cbf3d6865e210e28dd
mnehro <mnehro@madfoat.com>
2024-06-10 14:05:28 +0300

    Design & Fetch countries

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,66 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { heart } from 'ionicons/icons';
+import { CountryService } from '../services/country.service';
+
+addIcons({
+  'heart': heart
+});
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, NgFor],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  public letters: string[] = [];
+  public isHomePage: boolean = true;
+  public countries: any[] = [];
+  public groupedCountries: any = {};
+
+
+  constructor(private countryService: CountryService) { }
+
+  ngOnInit(): void {
+    this.countryService.getCountries().subscribe(result => {
+      if (!result.error) {
+        this.countries = result.data;
+        this.groupCountries();
+        this.createLetters();
+      }
+      console.log(this.groupedCountries);
+
+      // this.sortCountries();
+    });
+  }
+  private groupCountries(): void {
+    this.countries.forEach(country => {
+      const letter = country.name.charAt(0).toUpperCase();
+      if (!this.groupedCountries[letter]) {
+        this.groupedCountries[letter] = [];
+      }
+
+      if (country.iso3 !== 'ISR') {
+        this.groupedCountries[letter].push(country);
+      }
+    });
+  }
+  createLetters() {
+    this.letters = Object.keys(this.groupedCountries).sort();
+  }
+
+  public toggleView(): void {
+    this.isHomePage = !this.isHomePage;
+  }
+  public viewMore(): void {
+    console.log("population");
+
+  }
+  public onImageError(event: any) {
+    event.target.src = 'assets/default-flag.png'; 
+  }
 }
