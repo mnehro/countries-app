3fa752c3ba922e306727023d08b32c245a70608b
mnehro <mnehro@madfoat.com>
2024-06-12 15:39:31 +0300

    Refactor

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PopulationResult, Result } from '../models/result.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
-  private baseUrl = 'https://countriesnow.space/api/v0.1/countries';
+  private baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
